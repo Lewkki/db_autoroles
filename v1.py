33c0be61a400db26918ecada8188b33575fdecfe
Luke <lukehickey08@gmail.com>
2025-12-02 18:43:40 -0800

    several workers

--- v1.py
+++ v1.py
@@ -1,15 +1,23 @@
 import random
 
 roles = ["window", "shots", "milk", "linebuster"]
+workers = ["Luke"]
 
 shift_start = int(input("When does your shift start? (24h): "))
 shift_end = int(input("When does your shift end? (24h): "))
+num_workers = int(input("How many people are on shift? "))
+
+for x in range(num_workers):
+    user_input = input("Enter the name of the worker: ")
+    workers.append(user_input)
 
 hours = shift_end - shift_start
 hour_count = 0.00
 
+
 for x in range(hours):
-    role = random.choice(roles)
     hour_count += 1.00
     print(f'Hour {hour_count}')
-    print(f'  {role}')
+    for worker in workers:
+        role = random.choice(roles)
+        print(f'  {worker} -> {role}')
