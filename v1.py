f6341c2acd1457ae5ecd22f817c29b260756fd4a
Luke Hickey <lukehickey08@gmail.com>
2025-12-08 19:31:31 -0800

    Added roles based on workers

--- v1.py
+++ v1.py
@@ -1,11 +1,20 @@
 import random
 
 roles = ["window", "shots", "milk", "linebuster"]
-workers = ["Luke"]
+workers = []
 
 shift_start = int(input("When does your shift start? (24h): "))
 shift_end = int(input("When does your shift end? (24h): "))
 num_workers = int(input("How many people are on shift? "))
+
+if num_workers == 5:
+    roles = ["window", "shots", "milk", "linebuster", "pit"]
+elif num_workers == 4:
+    roles = ["window", "shots", "milk", "linebuster"]
+elif num_workers == 3:
+    roles = ["window", "shots", "milk"]
+elif num_workers == 2:
+    roles = ["window", "shots"]
 
 for x in range(num_workers):
     user_input = input("Enter the name of the worker: ")
@@ -13,7 +22,6 @@ for x in range(num_workers):
 
 hours = shift_end - shift_start
 hour_count = 0.00
-
 
 for x in range(hours):
     hour_count += 1.00
